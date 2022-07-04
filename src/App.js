b53493746d68f8964c70d1fa7a3f26bc8a39f1f9
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-05 00:28:12 +0530

    banner added without styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import Row from './Row'
 import requests from './request'
+import Banner from './Banner'
 
 export default function App() {
   return (
     <div className='app'>
-      <Row isLargeRow={true} title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Banner />
+      <Row isLargeRow={true} title="Our Originals" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Top Action Movies" fetchUrl={requests.fetchActionMovies} />
     </div>
   )
---
+++ src/Banner.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import './Banner.css'
+import { instance } from './config'
+import requests from './request'
+
+export default function Banner() {
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            try {
+
+                const res = await instance.get(requests.fetchNetflixOriginals)
+
+                setMovie(res.data.results[
+                    Math.floor(Math.random() * res.data.results.length - 1)
+                ])
+
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        fetchMovie();
+    }, [])
+
+    console.log(movie);
+
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str
+    }
+
+    return (
+        <header className='banner'
+            style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`, backgroundSize: "cover", backgroundPosition: "center center" }}>
+
+            <div className='bannerContents'>
+                <h1 className='bannerTitle'>{movie.title || movie.name || movie.original_name}</h1>
+                <p>{truncate(movie.overview, 150)}</p>
+                <div className='buttons'>
+                    <button>Play</button>
+                    <button>More info</button>
+                </div>
+            </div>
+
+        </header>
+    )
+}
